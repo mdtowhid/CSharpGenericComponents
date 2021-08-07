fd761db85541f71c0e73e96b48555397e3c24678
Md towhid <newtowhid1232@gmail.com>
2021-08-07 23:20:19 +0600

    DynamicCSharpClassMaker_Added_7_8_2021

---
+++ GenericComponents/Controllers/DbSchemaController.cs
@@ -0,0 +1,164 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Database;
+using System.Data;
+using System.IO;
+using System.Web;
+using Microsoft.AspNetCore.Hosting;
+
+namespace GenericComponents.Controllers
+{
+    public class DbSchemaController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public DbSchemaController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+
+        public IActionResult Index()
+        {
+            ViewBag.Tables = ListTables();
+            List<List<string>> schemas = new List<List<string>>();
+            foreach (string tn in ViewBag.Tables)
+            {
+                schemas.Add(ListOfTableSchema(tn));
+            }
+
+            ViewBag.TablesSchemas = schemas;
+
+            bool c = CreateDirectory("DynamicFilesFolderPath");
+            if (c == false)
+            {
+                CreateAndWriteInFiles(ViewBag.Tables, schemas);
+            }
+            GetAllFilesNameFromFolder("DynamicFilesFolderPath");
+            return View();
+        }
+
+        public List<string> ListTables()
+        {
+            SqlConnection _connection = DatabaseInfo.GetSqlConnection();
+            List<string> tables = new List<string>();
+            _connection.Open();
+            DataTable dt = _connection.GetSchema("Tables");
+            foreach (DataRow row in dt.Rows)
+            {
+                string tablename = (string)row[2];
+                tables.Add(tablename);
+            }
+            _connection.Close();
+            return tables;
+        }
+
+        public List<string> ListOfTableSchema(string tableName)
+        {
+            SqlConnection _connection = DatabaseInfo.GetSqlConnection();
+            List<string> tableSchemas = new List<string>();
+            _connection.Open();
+
+
+            string q = @"SELECT COLUMN_NAME AS ColName, DATA_TYPE AS DataType
+                FROM INFORMATION_SCHEMA.COLUMNS
+                WHERE TABLE_NAME = '"+tableName+"' order by COLUMN_NAME ASC";
+
+            SqlCommand cmd = DatabaseInfo.GetSqlCommand(q, _connection);
+            using(SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    tableSchemas.Add(reader["ColName"].ToString() +"_"+ reader["DataType"].ToString());
+                }
+            }
+
+            _connection.Close();
+
+            return tableSchemas;
+        }
+
+        public string[] GetAllFilesNameFromFolder(string folderPath)
+        {
+            string c =  _webHostEnvironment.WebRootPath;
+            string combined = Path.Combine(c, folderPath);
+            string[] filePaths = new string[] { };
+            if (Directory.Exists(combined))
+            {
+                filePaths = Directory.GetFiles(c);
+            }
+            else
+            {
+                Directory.CreateDirectory(combined);
+            }
+
+            return filePaths;
+        }
+
+        public bool CreateDirectory(string folderPath)
+        {
+            string c = _webHostEnvironment.WebRootPath;
+            string combined = Path.Combine(c, folderPath);
+            DirectoryInfo di = null;
+            if (!Directory.Exists(combined))
+            {
+                di = Directory.CreateDirectory(combined);
+            }
+
+            return di != null ? true : false;
+        }
+
+        public void CreateAndWriteInFiles(List<string> fileNames, List<List<string>> tableSchemas)
+        {
+            string c = _webHostEnvironment.WebRootPath + "\\DynamicFilesFolderPath";
+
+            foreach (string fileName in fileNames)
+            {
+                string pathString = Path.Combine(c, fileName + ".cs");
+                foreach (List<string> tblSchema in tableSchemas)
+                {
+                    string props = "";
+                    foreach (string column in tblSchema)
+                    {
+                        string[] sp = column.Split('_');
+                        string type = sp[1];
+                        string name = sp[0];
+                        props += "public " + GetActualType(type) + " " + name + " { get; set; }\n";
+                    }
+
+                    System.IO.File.WriteAllText(pathString, props);
+                }
+            }
+        }
+
+        private string GetActualType(string type)
+        {
+            type = type.ToLower();
+            switch (type)
+            {
+                case "nvarchar":
+                    type = "string";
+                    break;
+                case "varchar":
+                    type = "string";
+                    break;
+                case "bigint":
+                    type = "long";
+                    break;
+                case "bit":
+                    type = "bool";
+                    break;
+                case "datetime":
+                    type = "DateTime";
+                    break;
+                default:
+                    break;
+            }
+
+            return type;
+        }
+    }
+}
---
+++ GenericComponents/wwwroot/DynamicFilesFolderPath/Doctors.cs
@@ -0,0 +1,288 @@
+public class Doctors
+{	public bool Active { get; set; }
+	public string AdminRole { get; set; }
+	public string ConfirmedPassword { get; set; }
+	public string Details { get; set; }
+	public string Email { get; set; }
+	public string FirstName { get; set; }
+	public string LastName { get; set; }
+	public string Password { get; set; }
+	public string PhoneNumber { get; set; }
+	public string PhotoPath { get; set; }
+	public long UserId { get; set; }
+
+}	public bool Active { get; set; }
+	public long Id { get; set; }
+	public string PanelControllerName { get; set; }
+	public string RoleName { get; set; }
+
+}	public bool Active { get; set; }
+	public string CategoryName { get; set; }
+	public long Id { get; set; }
+
+}	public DateTime AccountCreationDate { get; set; }
+	public DateTime AccountUpdationDate { get; set; }
+	public string ConformPassword { get; set; }
+	public string CustomerUniqueCode { get; set; }
+	public string Email { get; set; }
+	public string EmailConfirmationCode { get; set; }
+	public long Id { get; set; }
+	public bool IsEmailConfirmed { get; set; }
+	public string Password { get; set; }
+	public string UserName { get; set; }
+
+}	public string DesgnationName { get; set; }
+	public int Id { get; set; }
+
+}	public string BlogCategoryName { get; set; }
+	public string Details { get; set; }
+	public string Heading { get; set; }
+	public long Id { get; set; }
+	public bool IsActive { get; set; }
+	public string PhotoPath { get; set; }
+	public DateTime PostedDate { get; set; }
+	public DateTime UpdatedDate { get; set; }
+	public long UserId { get; set; }
+
+}	public bool Active { get; set; }
+	public long AddedBy { get; set; }
+	public string AdvertisementPageLocation { get; set; }
+	public float Cost { get; set; }
+	public long Id { get; set; }
+	public string PageDescriptions { get; set; }
+	public string PageName { get; set; }
+
+}	public string Address { get; set; }
+	public string AdvDetails { get; set; }
+	public long CategoryId { get; set; }
+	public string ContactNo { get; set; }
+	public string Heading { get; set; }
+	public long Id { get; set; }
+	public bool IsActive { get; set; }
+	public bool IsSold { get; set; }
+	public string PhotoPath { get; set; }
+	public float Price { get; set; }
+	public long ReviewId { get; set; }
+	public long UserId { get; set; }
+
+}	public string AdvDetails { get; set; }
+	public float Cost { get; set; }
+	public string CostStatus { get; set; }
+	public DateTime ExpirationDate { get; set; }
+	public DateTime FromDate { get; set; }
+	public int HowManyDays { get; set; }
+	public long Id { get; set; }
+	public bool IsActive { get; set; }
+	public string PageLocation { get; set; }
+	public string PageName { get; set; }
+	public string PhotoPath { get; set; }
+	public long UserId { get; set; }
+	public long VisitorsClicks { get; set; }
+
+}	public string CategoryName { get; set; }
+	public long Id { get; set; }
+
+}	public bool Active { get; set; }
+	public long AdvertisementId { get; set; }
+	public int Counter { get; set; }
+	public string Details { get; set; }
+	public long Id { get; set; }
+	public string ThumbImagePath { get; set; }
+	public long UserId { get; set; }
+
+}	public bool Active { get; set; }
+	public string CategoryName { get; set; }
+	public long Id { get; set; }
+	public long ParentId { get; set; }
+
+}	public string BloodGroupName { get; set; }
+	public long Id { get; set; }
+
+}	public string Address { get; set; }
+	public string BloodGroupName { get; set; }
+	public string ContactNo { get; set; }
+	public long Id { get; set; }
+	public bool IsActive { get; set; }
+	public DateTime LastDateBloodGiven { get; set; }
+	public DateTime NextTimeCan { get; set; }
+	public long UserId { get; set; }
+
+}	public int Id { get; set; }
+	public string SizeName { get; set; }
+
+}	public string AccountCreationDate { get; set; }
+	public string AccountUpdationDate { get; set; }
+	public long CompanyId { get; set; }
+	public string CompanyUniqueCode { get; set; }
+	public string ConfirmPassword { get; set; }
+	public string Email { get; set; }
+	public bool IsActiveAccount { get; set; }
+	public bool IsConfirmedEmail { get; set; }
+	public bool MadeCall { get; set; }
+	public string Password { get; set; }
+	public string UserName { get; set; }
+
+}	public bool Active { get; set; }
+	public long Id { get; set; }
+	public long ParentCategoryId { get; set; }
+	public string SubCategoryName { get; set; }
+
+}	public long CompanyId { get; set; }
+	public long CustomerId { get; set; }
+	public long Id { get; set; }
+	public long ProductId { get; set; }
+	public long Quantity { get; set; }
+	public bool Status { get; set; }
+	public float TotalPrice { get; set; }
+
+}	public bool Active { get; set; }
+	public string CategoryName { get; set; }
+	public long Id { get; set; }
+
+}	public string Activities { get; set; }
+	public string Address { get; set; }
+	public string CentreName { get; set; }
+	public string ContactNo { get; set; }
+	public long Id { get; set; }
+	public bool IsActive { get; set; }
+	public string PhotoPath { get; set; }
+	public long UserId { get; set; }
+
+}	public long CategoryId { get; set; }
+	public string Details { get; set; }
+	public float DiscountPrice { get; set; }
+	public long Id { get; set; }
+	public bool IsActive { get; set; }
+	public string Person { get; set; }
+	public string PhotoPath { get; set; }
+	public float Price { get; set; }
+	public string ProductName { get; set; }
+	public long Quantity { get; set; }
+	public long Sort { get; set; }
+	public long SubCategoryId { get; set; }
+	public long TotalArrivedQuantity { get; set; }
+	public string UpdatedAt { get; set; }
+	public long UpdatedBy { get; set; }
+
+}	public long CategoryId { get; set; }
+	public string CategoryName { get; set; }
+	public long CompanyId { get; set; }
+	public long Id { get; set; }
+
+}	public bool ActiveStatus { get; set; }
+	public long DesignationId { get; set; }
+	public string Details { get; set; }
+	public string Email { get; set; }
+	public string FullName { get; set; }
+	public long Id { get; set; }
+	public string PhotoPath { get; set; }
+
+}	public string DesignationName { get; set; }
+	public long Id { get; set; }
+
+}	public string ContactNo { get; set; }
+	public string Designation { get; set; }
+	public string DoctorName { get; set; }
+	public string FloorNo { get; set; }
+	public string HospitalName { get; set; }
+	public long Id { get; set; }
+	public bool IsActive { get; set; }
+	public string RoomNo { get; set; }
+	public string TntNumber { get; set; }
+	public long UserId { get; set; }
+
+}	public string Address { get; set; }
+	public string City { get; set; }
+	public string ContactAt { get; set; }
+	public string ContactNumber { get; set; }
+	public long CustomerId { get; set; }
+	public string FullName { get; set; }
+	public long Id { get; set; }
+	public string OrderId { get; set; }
+	public string ZipCode { get; set; }
+
+}	public string Address { get; set; }
+	public string HelpLine { get; set; }
+	public string HospitalName { get; set; }
+	public long Id { get; set; }
+	public bool IsActive { get; set; }
+	public long UserId { get; set; }
+
+}	public string Activities { get; set; }
+	public string Address { get; set; }
+	public string ContactNo { get; set; }
+	public long Id { get; set; }
+	public string InstituteName { get; set; }
+	public string InstituteType { get; set; }
+	public bool IsActive { get; set; }
+	public long UserId { get; set; }
+
+}	public long CategoryId { get; set; }
+	public long CustomerId { get; set; }
+	public long Id { get; set; }
+	public string OrderedTime { get; set; }
+	public string OrderId { get; set; }
+	public long ProductId { get; set; }
+	public string ShippingStatus { get; set; }
+	public long SubCategoryId { get; set; }
+	public float TotalPrice { get; set; }
+	public int TotalQuantity { get; set; }
+
+}	public long Id { get; set; }
+	public string InstituteTypeName { get; set; }
+
+}	public long Id { get; set; }
+	public long ParentId { get; set; }
+	public string ReportCategoryName { get; set; }
+
+}	public long CustomerId { get; set; }
+	public long Id { get; set; }
+	public string TagName { get; set; }
+
+}	public string AccessPanel { get; set; }
+	public string AdminUniqueCode { get; set; }
+	public string ConfirmationCode { get; set; }
+	public string Email { get; set; }
+	public int Id { get; set; }
+	public bool IsEmailConfirmed { get; set; }
+	public DateTime LoginDateTime { get; set; }
+	public string Password { get; set; }
+
+}	public long CompanyId { get; set; }
+	public long CustomerId { get; set; }
+	public long Id { get; set; }
+	public string IsProblemSolved { get; set; }
+	public long ProductId { get; set; }
+	public long ReportCategoryId { get; set; }
+	public string ReportDetails { get; set; }
+	public string ReportedBy { get; set; }
+	public DateTime ReportingDate { get; set; }
+
+}	public bool AdminApproval { get; set; }
+	public string ContactNo { get; set; }
+	public string Designation { get; set; }
+	public long Id { get; set; }
+	public string InstituteName { get; set; }
+	public string Subjects { get; set; }
+	public string TeacherName { get; set; }
+	public long UserId { get; set; }
+
+}	public long Id { get; set; }
+	public string TeacherDesignationName { get; set; }
+
+}	public DateTime AccountCreationDate { get; set; }
+	public DateTime AccountUpdationDate { get; set; }
+	public string Address { get; set; }
+	public string ConfirmPassword { get; set; }
+	public string Details { get; set; }
+	public string Email { get; set; }
+	public long Id { get; set; }
+	public bool IsActiveAccount { get; set; }
+	public bool IsConfirmedEmail { get; set; }
+	public string Password { get; set; }
+	public string PhotoPath { get; set; }
+	public string TransactionMethodStatus { get; set; }
+	public string UserName { get; set; }
+	public string UserUniqueCode { get; set; }
+
+}
